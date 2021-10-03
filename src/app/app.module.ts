429bf50ff4a4bcab6be95c493f76988e4f3751db
Kristiyan G Zlatkovski <52286225+agunimonx@users.noreply.github.com>
2021-10-04 01:16:20 +0300

    Update app.module.ts
    imported forms Module to the main module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseComponent } from './course/course.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, CourseComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [CourseService],
   bootstrap: [AppComponent],
 })
